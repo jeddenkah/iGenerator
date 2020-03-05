a810dc3e5a47d7ad15c53eced3813f905d3622c8
ifeniprun16 <ifeniprun16@gmail.com>
2020-03-05 14:06:24 +0700

    add features (generate a,u,e,o)

--- i-generator.js
+++ i-generator.js
@@ -1,10 +1,18 @@
 
-function iGenerator() {
+var pilihan = document.querySelector('input[name="generate"]:checked').value
+function handleClick(radio){
+    pilihan = radio.value
+    iGenerator(pilihan)
+}
+document.getElementById('kataAwal').addEventListener("keyup", function () {
+    iGenerator(pilihan)
+
+});
+function iGenerator(huruf) {
     var kataAwal = document.getElementById("kataAwal").value
     var kataAkhir = kataAwal
-    var penggantiLowerCase = "i"
-    var penggantiUppercase = "I"
-
+    var penggantiLowerCase = huruf.toLowerCase()
+    var penggantiUppercase = huruf.toUpperCase()
     kataAkhir = kataAkhir.replace(/A/g, penggantiUppercase)
     kataAkhir = kataAkhir.replace(/I/g, penggantiUppercase)
     kataAkhir = kataAkhir.replace(/U/g, penggantiUppercase)
@@ -15,7 +23,5 @@ function iGenerator() {
     kataAkhir = kataAkhir.replace(/u/g, penggantiLowerCase)
     kataAkhir = kataAkhir.replace(/e/g, penggantiLowerCase)
     kataAkhir = kataAkhir.replace(/o/g, penggantiLowerCase)
-
     document.getElementById("hasilKata").value = kataAkhir
 }
-
